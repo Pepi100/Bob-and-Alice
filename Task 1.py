c6cc335a11ca7667b1a8b6fedc05a7f7d9e1e70e
993m <cioclov_maria@yahoo.com>
2024-01-11 22:23:49 +0200

    Code refactoring

--- Task 1.py
+++ Task 1.py
@@ -1,21 +1,22 @@
 import hashlib
 import os
 
-
 def getDivisorsPair(number):
     divisors = []
     i = 1
-    while i*i <= number:
+    while i * i <= number:
         if number % i == 0:
-            divisors.append([i, number//i])
+            divisors.append([i, number // i])
         i += 1
     return divisors
+
 
 def sha256(input):
     sha256_hash_obj = hashlib.sha256()
     sha256_hash_obj.update(input.encode("utf-8"))
     hashed_input = sha256_hash_obj.hexdigest()
     return hashed_input
+
 
 nr_pixels = []
 
@@ -28,14 +29,13 @@ headers = ["602a4a8fff652291fdc0e049e3900dae608af64e5e4d2c5d4332603c9938171d",
            "bd0fd461d87fba0d5e61bed6a399acdfc92b12769f9b3178f9752e30f1aeb81d",
            "372df01b994c2b14969592fd2e78d27e7ee472a07c7ac3dfdf41d345b2f8e305"]
 
-
 # collect aproximate number of pixels in each image by reading the encrypted file's size
 files_folder = 'task1'
 for file in os.listdir(files_folder):
     file_path = os.path.join(files_folder, file)
     with open(file_path, 'r', encoding='latin-1') as f:
         content = f.read()
-        nr_pixels.append(len(content)//3)
+        nr_pixels.append(len(content) // 3)
 
 # since Present cipher is a block cipher, it encrypts data in 64-bit chunks (8 bytes)
 # therefore, it is possible that the file has been padded up to a file size that is a multiple of 8 bytes
@@ -59,5 +59,5 @@ for image_index in range(len(nr_pixels)):
 
         for j in range(len(possible_hashed_headers)):
             if possible_hashed_headers[j] in headers:
-                print(f"Image: {image_index}, Header number: {headers.index(possible_hashed_headers[j])}, Header: {possible_hashed_headers[j]}, Dimensions: {dimensions[j][0]} x {dimensions[j][1]} px")
-
+                print(
+                    f"Image: {image_index}, Header number: {headers.index(possible_hashed_headers[j])}, Header: {possible_hashed_headers[j]}, Dimensions: {dimensions[j][0]} x {dimensions[j][1]} px")
--- Task 2.py
+++ Task 2.py
@@ -8,18 +8,21 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 import time
 
-# INITIALIZARE FISIERE
+
+# CREATE FILES
 def generate_random_size():
     width = random.randint(200, 800)
     height = random.randint(200, 800)
     return width, height
 
+
 def generate_image_with_letter(letter):
     image_size = generate_random_size()
     image = Image.new('RGB', image_size)
     draw = ImageDraw.Draw(image)
-    draw.text((100,100), letter)
+    draw.text((100, 100), letter)
     return image
+
 
 def sha256_hash(input):
     sha256_hash_obj = hashlib.sha256()
@@ -28,63 +31,97 @@ def sha256_hash(input):
     return hashed_input
 
 
-sizes = [(346, 447), (548, 553), (708, 557), (222, 416), (236, 528)]
-headers = ['86dcedb09f344f6afc28ab52b81989a5b37c7265cc7930c3249ec089f339d720',
-           '3273584382cdf68bd84f00047124b559cd6c6690516986c459481042f8464b07',
-           'ffca5a23b88643fa3950af4491f7c330101fddf36b4e87818d65bb05ce738551',
-           '79575d3bec00973e030495b6bbac23e3f13903b138adda4f7559ad0ad2c6b99a',
-           'b0e5f6a8e998f160664436990e6abcf95e978242bf3fea8fb62da218a5ee0e51']
+def generate_files(letters):
+    random.seed(int(time.time()))
+    sizes = []
+    headers = []
+    letter_frequency = {}
+    for letter in letters:
+        if letter == ' ':
+            continue
+        letter_image = generate_image_with_letter(letter)
+        file_path = f'task2/original/{letter}'
+        if letter in letter_frequency:
+            letter_frequency[letter] += 1
+            letter_image.save(f'{file_path}{letter_frequency[letter]}.ppm', 'PPM')
+        else:
+            letter_frequency[letter] = 0
+            letter_image.save(f'{file_path}.ppm', 'PPM')
+        sizes.append(letter_image.size)
 
-# random.seed(int(time.time()))
-# for letter in "MARIA":
-#     letter_image = generate_image_with_letter(letter)
-#     letter_image.save(f'{letter}.ppm', 'PPM')
-#     sizes.append(letter_image.size)
+    for size in sizes:
+        hashed_header = sha256_hash(f"P6 {size[0]} {size[1]} 255")
+        headers.append(hashed_header)
+
+    return sizes, headers
+
+
+message = "RADU SI MARIA"
+# sizes, headers = generate_files(message)
 # print(sizes)
-# for size in sizes:
-#     hashed_header = sha256_hash(f"P6 {size[0]} {size[1]} 255")
-#     headers.append(hashed_header)
 # print(headers)
 # exit(0)
 
-# CRIPTARE FISIERE
-# key = os.urandom(16)
-# cipher = Cipher(algorithms.AES(key), modes.ECB())
-#
-# for letter in "MARIA":
-#     file_path = f"{letter}.ppm"
-#     with open(file_path, 'rb') as file:
-#         plaintext = file.read()
-#
-#         encryptor = cipher.encryptor()
-#         padder = padding.PKCS7(algorithms.AES.block_size).padder()
-#         padded_data = padder.update(plaintext) + padder.finalize()
-#         ciphertext = encryptor.update(padded_data) + encryptor.finalize()
-#     with open(f"encrypted_{letter}.ppm", 'wb') as encrypted_file:
-#         encrypted_file.write(ciphertext)
-#
+
+sizes = [(314, 763), (242, 554), (424, 472), (595, 702), (343, 586), (229, 631), (356, 700), (719, 253), (736, 298), (694, 270), (608, 499)]
+headers = ['2344400b83bac31695af8a1b03e84df001179ae73e2bd9701097f6f156f00d03',
+           'fef056f513776e434b7a7983c1a6355217f0734c3dfc964d96c993c4bbf133d4',
+           'e79ebf9d8650ac463fd8c493c0f0f4b349f11a3fbdb21e4b2e345a6bcf386975',
+           '78df4f389c89aeac082a3d064e9aabf594bb94f29557150dfcc08f689c6274fe',
+           'f4e75ec2ccfc505d5676cc93021b23e966508e55e577d29b3a411e513cfc8ffa',
+           'c402c4a36ac2a3f7f3dd97ca44acbf90dbd081e1352eb2a07f54067c3eac922d',
+           '7e81447e86d1fb949f67301d0a406a0720482ae0a627cf1c9adc758a463ba173',
+           '0a4b2afc4528881ac4f8317ae0f08752428fbe5cfc52852d357a233f02db9164',
+           '99fcb00fca3e2e2c6e98f1f854213de9c99ef4a1ca7a5c1d07c97a4580a8eb5c',
+           'c73c0398382266fe308bb4616f4a0a9e9e50cebe1eb9bf90fd46dd80369f6f96',
+           '86cca9f69993671e6212a6d69e6235068c806918a05b7f043fe220d95ce78292']
+
+
+# ENCRYPT FILES
+def encrypt_files():
+    key = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.ECB())
+
+    folder_path = 'task2/original'
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        with open(file_path, 'rb') as file:
+            plaintext = file.read()
+            encryptor = cipher.encryptor()
+            padder = padding.PKCS7(algorithms.AES.block_size).padder()
+            padded_data = padder.update(plaintext) + padder.finalize()
+            ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+        with open(f'task2/encrypted/{filename}', 'wb') as encrypted_file:
+            encrypted_file.write(ciphertext)
+
+
+# encrypt_files()
 # exit(0)
-# REZOLVAREA
+
+
+# SOLVE THE PROBLEM
 def grtPairsOfDivisors(number):
     divisors = []
     i = 1
-    while i*i <= number:
+    while i * i <= number:
         if number % i == 0:
-            divisors.append([i, number//i])
+            divisors.append([i, number // i])
         i += 1
     return divisors
 
-nr_pixels = []
 
-for letter in "MARIA":
-    file_path = f"encrypted_{letter}.ppm"
-    with open(file_path, 'rb') as f:
+nr_pixels = []
+files_folder = 'task2/original'
+for file in os.listdir(files_folder):
+    file_path = os.path.join(files_folder, file)
+    with open(file_path, 'r', encoding='latin-1') as f:
         content = f.read()
-        nr_pixels.append(len(content)//3)
+        nr_pixels.append(len(content) // 3)
+
 
 for image_index in range(len(nr_pixels)):
-    for padding_length in range(16):
-        possible_dimensions = grtPairsOfDivisors(nr_pixels[image_index]-padding_length)
+    for padding_length in range(6):
+        possible_dimensions = grtPairsOfDivisors(nr_pixels[image_index] - padding_length)
         possible_hashed_headers = []
         dimensions = []
         for dimension in possible_dimensions:
@@ -99,6 +136,5 @@ for image_index in range(len(nr_pixels)):
 
         for j in range(len(possible_hashed_headers)):
             if possible_hashed_headers[j] in headers:
-                print(image_index, possible_hashed_headers[j])
-                print(dimensions[j])
-    print()
+                print(
+                    f"Image: {image_index}, Header number: {headers.index(possible_hashed_headers[j])}, Header: {possible_hashed_headers[j]}, Dimensions: {dimensions[j][0]} x {dimensions[j][1]} px")
